b7a7c4c4133deb8cbf82951e1ed84023ab704bf6
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-04-09 19:44:12 +0700

    update new folder

---
+++ Javascript/lecture.js
@@ -0,0 +1,59 @@
+// console.log("Hello World");
+// console.log("Hehe");
+// console.log(10);
+
+// String => chuỗi ký tự là tập hợp các ký tự viết dc viết cạnh nhau, vd: "hkjn@#$^%%$"
+// Number => số
+// Boolean => true/false
+// undefined => chua dc dinh nghia
+// null
+
+// 1. Cách để khai báo 1 biến trong JS (let, const, var);
+// let tên biến = giá trị
+
+// let myName = "Minh";
+// myName = "Phát";
+// myName = "Ninh";
+// myName = 10;
+// myName = 1000;
+// console.log(typeof myName);
+
+// let check1 = false;
+// console.log(typeof check1);
+
+// let check2 = "true";
+// console.log(typeof check2);
+
+// let check3 = "10";
+// console.log(typeof check3);
+
+// let check4 = 10;
+// console.log(typeof check4);
+
+// let myName = "Nguyễn Danh Phương";
+// console.log("Tôi tên là " + myName);
+
+// Chữa bài:
+// let myName = "Phương"; // không thể khai báo tên biến trùng nhau
+// let myAge = 22;
+// let doHomework = true;
+
+// console.log("Tên của tôi là " + myName);
+// console.log("Năm nay tôi " + myAge + " tuổi");
+// console.log("Trạng thái bài tập về nhà của tôi là: " + doHomework);
+
+// Cách 1
+// let text1 = "Tên của tôi là " + myName;
+// let text2 = ". Năm nay tôi " + myAge + " tuổi";
+// let text3 = ". Trạng thái bài tập về nhà của tôi là: " + doHomework;
+// console.log(text1 + text2 + text3);
+
+// console.log(
+//   "Tên của tôi là " +
+//     myName +
+//     ". Năm nay tôi " +
+//     myAge +
+//     " tuổi" +
+//     ". Trạng thái bài tập về nhà của tôi là: " +
+//     doHomework
+// );
---
+++ JavascriptBackupLecture/app.js
@@ -0,0 +1,30 @@
+// console.log("Đức Mạnh bị hâm");
+// console.log("Thầy Phương dzai");
+
+// 1. Kiểu dữ liệu:
+// String: chuỗi kí tự ghép lại với nhau: "!@#$%#^$%^$%^sdkhjfbsdjhfbhsdjfwefwef"
+// Number: chứa mỗi số: 1,2,3,4 12388 =>>>>>>>>>>>>>>   10 KHÁC SO VỚI "10"
+// Boolean: true/false
+
+// 2. Cách để khai báo biến với let, const
+
+// let a = "Phương";
+// a = "Mạnh";
+// a = 100;
+// console.log(typeof a);
+
+// let b = 10;
+// console.log(typeof b);
+
+// let c = true;
+// console.log(typeof c);
+
+// const age = 22;
+// console.log(age);
+
+let name = "Phương";
+// let text = "Hello " + name;
+console.log("Hello " + name);
+
+let countPlus = 10;
+console.log("5 + 5 = " + countPlus);
